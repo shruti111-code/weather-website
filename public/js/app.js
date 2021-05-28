ca44404af2c1eee2baaaffab6e300d9aa812cbac
shruti110 <60483540+shruti110@users.noreply.github.com>
2021-05-28 19:54:12 +0530

    herok

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ wform.addEventListener('submit', (e) => {
     const location=search.value
     m1.textContent = 'LOADING...'
     m2.textContent = ''
-    fetch('http://localhost:3000/weather?address=' + location  ).then((response) => {
+    fetch('/weather?address=' + location  ).then((response) => {
     response.json().then((data) => {
         if(data.error)
         m1.textContent=data.error
